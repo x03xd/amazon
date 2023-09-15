670223bb410255f2a699e2a074d5e064b5279c2b
x03 <darkpower289@gmail.com>
2023-09-15 07:31:29 +0200

    commit

--- amazon/celery.py
+++ amazon/celery.py
@@ -1,21 +1,8 @@
-<<<<<<< HEAD
-from __future__ import absolute_import, unicode_literals
-=======
->>>>>>> dockerizing-2
 import os
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "amazon.settings")
 
-<<<<<<< HEAD
-app = Celery('amazon', backend='rpc://', broker='amqp://guest@localhost//')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.conf.broker_url = 'pyamqp://guest@localhost//' 
-app.conf.result_backend = 'rpc://'
-
-app.autodiscover_tasks()
-
-=======
 app = Celery('amazon')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
@@ -25,4 +12,3 @@ app.conf.result_backend = 'rpc://'
 app.conf.result_persistent = False  
 
 app.autodiscover_tasks()
->>>>>>> dockerizing-2
--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-def run_command(config):
-    location = config["location"]
-    command = config["command"]
-
-    process = subprocess.Popen(command, cwd=location, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    process.communicate()
-
