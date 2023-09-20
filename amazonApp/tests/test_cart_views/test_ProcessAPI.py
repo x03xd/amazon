bc958f33c47990df741ea343af99cfd80cb6cae4
x03 <darkpower289@gmail.com>
2023-09-20 12:56:19 +0200

    commit

--- amazonApp/tests/test_cart_views/test_ProcessAPI.py
+++ amazonApp/tests/test_cart_views/test_ProcessAPI.py
@@ -12,24 +12,27 @@ from amazonApp.views_folder.cart_views import ProcessAPI
 def api_client():
     return APIClient()
 
+@pytest.fixture
+def process_instance():
+    return ProcessAPI()
+
 
 @pytest.mark.django_db
 class TestProcessAPI:
 
     @pytest.mark.parametrize('quantity, product_quantity, total_quantity, exception', [
-        (12, 10, 3, 'Quantity exceeds available stock'),
-        (12, 13, 3, 'Quantity is not in range 1-10'),
-        (3, 13, 8, 'Maximum quantity of single item exceeded'),
+        (12, 10, 3, {"status": False, "info": "Quantity exceeds available stock"}),
+        (12, 13, 3, {"status": False, "info": "Quantity is not in the range of 1-10"}),
+        (3, 13, 8, {"status": False, "info": "Maximum quantity of your cart items exceeded"}),
         (3, 4, 4, None),
     ])
-
-    def test_validate_conditions(self, quantity, product_quantity, total_quantity, exception):
-        valid, response = self.validate_conditions(quantity, product_quantity, total_quantity)
+    def test_validate_conditions(process_api_instance, quantity, product_quantity, total_quantity, exception, process_instance):
+        valid, response =  process_instance.validate_conditions(quantity, product_quantity, total_quantity)
 
         if not valid:
             assert response == exception
         else:
-            assert response.data == exception
+            assert response is None
 
 
     def test_post_200(self, create_cartItem, create_product, create_user, create_cart, api_client):
@@ -39,17 +42,17 @@ class TestProcessAPI:
         create_cart
 
         url = reverse('process')
-        data = {'product_id': product.id, 'user_id': user.id, 'quantity': 999}
+        data = {'product_id': product.id, 'user_id': user.id, 'quantity': 1}
 
         response = api_client.post(url, data, format='json')
 
         assert response.status_code == status.HTTP_200_OK
-        assert response.data == {"status": True}
+        assert response.data == {"status": True, "info": "Produkt pomyślnie dodano do koszyka"}
 
 
     @pytest.mark.parametrize('mock_object, exception', [
-    (User, User.DoesNotExist('Simulating 404 status error -> no user')),
-    (Product, Product.DoesNotExist('Simulating 404 status error -> no product')),
+        (User, User.DoesNotExist('Simulating 404 status error -> no user')),
+        (Product, Product.DoesNotExist('Simulating 404 status error -> no product')),
     ])
     def test_post_404_no_user(self, mock_object, exception, create_cartItem, create_product, create_user, create_cart, api_client):
         with patch(f'amazonApp.views_folder.cart_views.{mock_object.__name__}.objects.get') as mock_post:
